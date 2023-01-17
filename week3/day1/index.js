a003a68aa00ff933195f97a71d3b4b50a732af7c
camveff <120936059+camveff@users.noreply.github.com>
2023-01-17 02:12:39 +0200

    Add files via upload

---
+++ week3/day1/index.js
@@ -0,0 +1,163 @@
+  // part1
+  let favouritefood= "burgers"
+  let favouritemeal= "dinner"
+  console.log("i eat ${favouritefood} at every ${favouritemeal}");
+
+
+  const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+
+
+  const myWatchedSeriesLength = myWatchedSeries.length
+
+  const myWatchedSeriesSentence = myWatchedSeries[0] + ", " + myWatchedSeries[1] " and " + myWatchedSeries[2]
+
+  console.log("myWatchedSeriesSentence:" , myWatchedSeriesSentence)
+
+
+
+  // part2
+
+
+
+  myWatchedSeries[2]= "friends"
+  console.log(myWatchedSeries)
+
+  myWatchedSeries.push("suits")
+  console.log(myWatchedSeries)
+
+  myWatchedSeries.unshift("breaking bad")
+  console.log(myWatchedSeries)
+
+  delete myWatchedSeries[1]
+
+  const moneyHeist= myWatchedSeries[2]
+  console.log(moneyHeist[2])
+
+  console.log(myWatchedSeries)
+
+  //part3
+
+
+
+
+  const celciusTemp = 20;
+
+  const farenheitTemp = celciusTemp / 5 * 9 + 32
+  console.log(farenheitTemp)
+
+
+  //part4
+
+
+
+  //prediction=55 since 34+21=55
+  outcome=55
+
+  //prediction=23 a=2 therfore 21+2 = 23
+  outcome=23
+
+  //prediction=undefined no given value
+  outcome= undefined
+
+  //prediction since 5 is a string the plus is seen as a concationation and therfore will be 75
+  outcome= 75
+
+
+
+  //part5
+
+  //preditction since potato is a string the result will be a 'number'
+  outcome='number'
+
+  //prediction since potato is a string the result will be a 'number'
+  outcome='number'
+
+  //prediction naN means not a number therefore it will be naN
+  outcome= 'number'
+
+  //prediction since potato is a string hello will also be a string
+  outcome= 'string'
+
+  //prediction since true is a type of boolean it will be a boolean
+  outcome= 'boolean'
+
+  //prediction this will be a boolean since it is the same 
+  outcome= 'boolean'
+
+  //prediction they are both strings and will ad the the word and letter together 'hamburgers'
+  outcome= 'hamburgers'
+
+  //prediction since there is a minus it will convert the string into 'hamburger'
+  outcome= 'naN'
+
+  //prediction the plus acts as a concatonation of the strings and will therfore be '13'
+  outcome= '13'
+
+  //prediction javascript is able to coerce the strings when there is a minus therefore '-2'
+  outcome= '-2'
+
+  //prediction 5 will become a string 'jonny5'
+  outcome= 'jonny5'
+
+  //prediction 5 is not a string an therefore it will be NaN
+  outcome= 'NaN'
+
+  //prediction cannot multiply a number by a string 'NaN'
+  outcome='NaN'
+
+  //prediction 1=1 therfore false
+  outcome= 'false'
+
+  //prediction == will compare any string to a number therfore it will be true
+  outcome= 'true'
+
+  //preditction === value is the same but type is not false
+  outcome= 'false' 
+
+
+  //part6
+
+  //prediction 5 will be coerced into a string 534
+  outcome='534'
+
+ //prediction 4 will be coerced into a number therefore 1
+ outcome= '1'
+
+ //prediction % will show remainder therefore 0
+ outcome= '0'
+
+ //prediction % will show remainder therefore 5
+ outcome= '5'
+
+ //prediction concationation of strings therefore it will be 'javascript'
+ outcome= 'javascript'
+
+ //prediction string with 2 empty spaces since both are still strings
+ outcome= '  '
+
+ //predction it will become a space and a zero because of concatination
+ outcome= ' 0'
+
+ //prediction concatintion of strings so truetrue
+ outcome= '2'
+
+ //prediction true plus false will be 1 since false will become 0 and true 1
+ outcome= '1'
+
+ //prediction true plus false will be 1 since false will become 0 and true 1
+ outcome= '1'
+
+ //prediction it will become -1 for the same reasons
+ outcome= '-1'
+
+ //prediction the ! changes the element to the opposite therefore it will become false
+ outcome= 'false'
+
+ //prediction this will become -1
+ outcome '-1'
+
+ //prediction since both are strings and being minused it will become NaN
+ outcome= 'NaN'
+
+
