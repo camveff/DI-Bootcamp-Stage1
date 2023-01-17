6461958cff746374d81ee9ee315bf23c5f074fcb
camveff <120936059+camveff@users.noreply.github.com>
2023-01-17 02:30:01 +0200

    Add files via upload

---
+++ week3/day1/daily challenge.js
@@ -0,0 +1,27 @@
+//exercise 1
+
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+array.shift()
+
+fruits.sort()
+
+fruits.push('kiwi')
+
+fruits.splice(0,1)
+
+fruits.reverse()
+
+
+
+
+console.log('fruits:', fruits)
+
+
+//exercrise 2
+
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1][0])
